118f6b4d74cf0d05b4f30f436cce8d3cb2380451
vicevirus <vicevirus@gmail.com>
2024-05-24 16:38:26 +0800

    challenges

--- challenge1/app/bot.js
+++ challenge1/app/bot.js
@@ -7,7 +7,7 @@ async function visitUrl(url) {
         }
 
         const browser = await puppeteer.launch({ 
-            headless: false,
+            headless: true,
             args: [
                 "--no-sandbox",
                 "--disable-setuid-sandbox",
